ed0b54074fa02472a686303a18a813f1a1531741
Tamás Köhler <tamas.kohler@leopoly.com>
2024-03-04 15:58:19 +0100

    Added project

---
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float MoveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("You can move with the W-A-S-D buttons!");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * MoveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * MoveSpeed;
+        transform.Translate(xValue,0,zValue);
+    }
+}
